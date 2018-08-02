57bed4d994e013d3b5dde44008bf8c7e5db1cfbc
Alexander Kraportov <alexk@tel-ar.co.il>
2018-08-02 10:34:35 +0300

    Login via passport finish

--- app.js
+++ app.js
@@ -54,11 +54,16 @@ app.use(session({
     saveUninitialized: true,
   }));
 
+//Passport midleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Global variables
 app.use((req,res,next)=>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
--- config/passport.js
+++ config/passport.js
@@ -9,12 +9,32 @@ const User = mongoose.model('users');
 
 module.exports = function(passport){
     passport.use(new LocalStrategy({usernameField: 'email'},(email,password,done)=>{
+        //Look for user in DB
         User.findOne({email:email})
         .then(user=>{
-            // if(!user){
-            //      return (null,fasle); 
-            // }
-            console.log(user);
+            if(!user){
+                 return done(null,false,{message: 'No user found'}); //Done() passport function (error,user, {error msg})
+            }
+
+            //Match password
+            bcrypt.compare(password,user.password, (err,isMatch)=>{
+                if (err) throw err;
+                if(isMatch){
+                    return done(null,user);
+                } else {
+                    return done(null,false,{message: 'Password incorect'});
+                }
+            });
         })
     }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+    });
 }
