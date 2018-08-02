286250a9002f5e6e9a1b55360b25f5a770279500
Alexander Kraportov <alexk@tel-ar.co.il>
2018-08-02 11:27:03 +0300

    Logout functionality added

--- routes/users.js
+++ routes/users.js
@@ -92,5 +92,12 @@ router.post('/register',(req,res) =>{
     
 });
 
+//Logout user
+router.get('/logout', (req,res)=>{
+    req.logout();
+    req.flash('success_msg','You are loged out');
+    res.redirect('/users/login');
+});
+
 
 module.exports = router;
